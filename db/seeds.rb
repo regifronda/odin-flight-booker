7a67b9f3e64ab3f7a33f61a3038696a5cd04f132
Regi Fronda <regisfronda@gmail.com>
2022-02-17 22:09:43 -0800

    Add SFO and NYC airports

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Airport.delete_all
+Airport.create([{ airport_code: 'SFO' }, { airport_code: 'NYC'}])
