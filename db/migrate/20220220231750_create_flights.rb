c57054c000105d5fe929156a077393d6bd5af911
Regi Fronda <regisfronda@gmail.com>
2022-02-20 18:18:32 -0800

    Add departure and arrival airport ids, start datetime, and flight duration columns

--- db/migrate/20220220231750_create_flights.rb
+++ db/migrate/20220220231750_create_flights.rb
@@ -1,6 +1,10 @@
 class CreateFlights < ActiveRecord::Migration[6.1]
   def change
     create_table :flights do |t|
+      t.integer :departure_airport_id
+      t.integer :arrival_airport_id
+      t.datetime :start_datetime
+      t.float :duration
 
       t.timestamps
     end
