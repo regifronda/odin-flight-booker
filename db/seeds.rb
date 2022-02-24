298d1b3942d0a491d569462257b6c231c34f9594
Regi Fronda <regisfronda@gmail.com>
2022-02-24 14:19:22 +1100

    Seed databse with flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Airport.delete_all
-Airport.create([{ airport_code: 'SFO' }, { airport_code: 'NYC'}, 
-  { airport_code: 'HND'}, { airport_code: 'MEL'}, { airport_code: 'SYD'}])
+Airport.reset_pk_sequence
+Airport.create([{ airport_code: 'SFO' }, { airport_code: 'NYC'}, { airport_code: 'HND'}, { airport_code: 'MEL'}, { airport_code: 'SYD'}])
+
+Flight.delete_all
+25.times do
+  (1..5).to_a.each do |i|
+    destinations = (1..5).to_a
+    destinations.delete(i)
+    Flight.create(departure_airport_id: i, arrival_airport_id: destinations.sample, start_datetime: DateTime.now + rand(100..600), duration: "05:30:00")
+  end
+end
+
+p "Created #{Airport.count} airport entries and #{Flight.count} flight entries."
