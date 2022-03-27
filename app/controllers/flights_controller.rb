9e0d896b02422d2f3487fb6debd9af9a3e1b4337
Regi Fronda <regisfronda@gmail.com>
2022-03-27 14:50:15 +1100

    Change @departure_airport_options to @airport_options

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,6 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all
-    @departure_airport_options = Airport.all.map { |d| [ d.airport_code, d.id ] }
+    @airport_options = Airport.all.map { |d| [ d.airport_code, d.id ] }
   end
 end
