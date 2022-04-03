8086f4016964b235e716dd53a38301f4a78ce088
Regi Fronda <regisfronda@gmail.com>
2022-04-03 17:25:18 +1000

    Add flight_date_formatted method

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, foreign_key: "departure_airport_id", class_name: "Airport"
   belongs_to :arrival_airport, foreign_key: "arrival_airport_id", class_name: "Airport"
+
+  def flight_date_formatted
+    self.start_datetime.strftime("%d/%m/%Y")
+  end
 end
