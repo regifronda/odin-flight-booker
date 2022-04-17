1a35f87bed8d643d75973b7ec43e06f2d7c300ca
Regi Fronda <regisfronda@gmail.com>
2022-04-17 12:25:40 +1000

    Add strong parameter helpers flight_params and departure_time_params

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,4 +3,14 @@ class FlightsController < ApplicationController
     @flights = Flight.all
     @airport_options = Airport.all.map { |d| [ d.airport_code, d.id ] }
   end
+
+  private
+
+  def flight_params
+    params.permit(:departure_airport_id, :arrival_airport_id)
+  end
+
+  def departure_time_params
+    params.permit(:start_datetime)
+  end
 end
